42ece965ab930c5aec7f8be88f0d5b1e404db5b5
Aman Tripathi <imamantripathi@gmail.com>
2023-03-04 15:16:52 +0530

    final commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// let audio = document.getElementById("audio");
+// let video = document.getElementById("video");
+// let mode = document.getElementsByClassName("mode");
+// let price_basic = document.getElementById("price-basic");
+// let price_advance = document.getElementById("price-advance");
+// let price_premium = document.getElementById("price-premium");
+// let number_of_session = document.getElementById("number-of-session");
+// let space = document.getElementById("space");
+// let img1 = document.getElementById("img-1");
+// let img2 = document.getElementById("img-2");
+// let img3 = document.getElementById("img-3");
+// let price_link_1 = document.getElementById("price-link-1");
+// let price_link_2 = document.getElementById("price-link-2");
+// let price_link_3 = document.getElementById("price-link-3");
+
+// video.addEventListener("click", () => {
+//   video.style.backgroundColor = "#3d3272";
+//   video.style.color = "#f5f5f5";
+//   audio.style.backgroundColor = "transparent";
+//   audio.style.color = "#171717";
+//   img1.src="images/plans-img/4.png";
+//   img2.src="images/plans-img/5.png";
+//   img3.src="images/plans-img/6.png";
+//   mode[0].textContent = "Mode -> Video Call";
+//   mode[1].textContent = "Mode -> Video Call";
+//   mode[2].textContent = "Mode -> Video Call";
+//   number_of_session.textContent = "3 Session";
+//   price_basic.textContent = "Rs. 1450.00/-";
+//   price_advance.textContent = "Rs. 2650.00/-";
+//   price_premium.textContent = "Rs. 3450.00/-";
+//   price_link_1.href="https://pages.razorpay.com/pl_LBVJjd1F0ONuzx/view";
+//   price_link_2.href="https://pages.razorpay.com/pl_LBVSSW5vZfoeKb/view";
+//   price_link_3.href="https://pages.razorpay.com/pl_LBVUZf4PhOgyLe/view";
+
+//   space.innerHTML = "</br>";
+//   space.style.display = "block";
+// });
+// audio.addEventListener("click", () => {
+//   audio.style.backgroundColor = "#3d3272";
+//   audio.style.color = "#f5f5f5";
+//   video.style.backgroundColor = "transparent";
+//   video.style.color = "#171717";
+//   img1.src="images/plans-img/1.png";
+//   img2.src="images/plans-img/2.png";
+//   img3.src="images/plans-img/3.png";
+//   mode[0].textContent = "Mode -> Audio Call";
+//   mode[1].textContent = "Mode -> Audio Call";
+//   mode[2].textContent = "Mode -> Audio Call";
+//   number_of_session.innerHTML =
+//     "3 Session <br> (2 Audio + 1 Video {Couple Session})";
+//   price_basic.textContent = "Rs. 950.00/-";
+//   price_advance.textContent = "Rs. 1650.00/-";
+//   price_premium.textContent = "Rs. 2900.00/-";
+//   price_link_1.href="https://pages.razorpay.com/pl_LBUc1vTLejqKbF/view";
+//   price_link_2.href="https://pages.razorpay.com/pl_LBVDc1nN7lFwgj/view";
+//   price_link_3.href="https://pages.razorpay.com/pl_LBVKgw2BLbZnEI/view";
+//   space.style.display = "none";
+// });
